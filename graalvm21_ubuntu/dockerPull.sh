5f1bb3a12ef5b265aa1dc9cd7a441fb7a24ca3e7
思追 <1679924785@qq.com>
2024-06-23 09:11:15 +0800

    build

--- graalvm21_ubuntu/dockerPull.sh
+++ graalvm21_ubuntu/dockerPull.sh
@@ -5,12 +5,12 @@ docker build -t jre:graalvm21_ubuntu22.build -f build.dockerfile . || exit
 docker build -t jre:graalvm21_ubuntu22.runtime -f runtime.dockerfile . || exit
 
 # 上传
-docker tag re:graalvm21_ubuntu22.build ccr.ccs.tencentyun.com/shaco_work/re:graalvm21_ubuntu22.build || exit
-docker push ccr.ccs.tencentyun.com/shaco_work/re:graalvm21_ubuntu22.build || exit
+docker tag jre:graalvm21_ubuntu22.build ccr.ccs.tencentyun.com/shaco_work/jre:graalvm21_ubuntu22.build || exit
+docker push ccr.ccs.tencentyun.com/shaco_work/jre:graalvm21_ubuntu22.build || exit
 
 docker tag jre:graalvm21_ubuntu22.runtime ccr.ccs.tencentyun.com/shaco_work/jre:graalvm21_ubuntu22.runtime || exit
 docker push ccr.ccs.tencentyun.com/shaco_work/jre:graalvm21_ubuntu22.runtime || exit
 
 # 删除上传成功
-docker rmi ccr.ccs.tencentyun.com/shaco_work/re:graalvm21_ubuntu22.build || exit
+docker rmi ccr.ccs.tencentyun.com/shaco_work/jre:graalvm21_ubuntu22.build || exit
 docker rmi ccr.ccs.tencentyun.com/shaco_work/jre:graalvm21_ubuntu22.runtime || exit
